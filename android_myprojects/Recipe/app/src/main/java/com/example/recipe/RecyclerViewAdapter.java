e304917913b7e236f94136f6682d465af85744dc
srujana-m <srujanamusti@gmail.com>
2021-04-19 23:51:10 +0530

    first commit

---
+++ android_myprojects/Recipe/app/src/main/java/com/example/recipe/RecyclerViewAdapter.java
@@ -0,0 +1,76 @@
+package com.example.recipe;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyHolder> {
+
+    private Context mContext;
+    private List<Recipes> mData;
+
+    public RecyclerViewAdapter(Context mContext,List<Recipes> mData)
+    {
+        this.mContext=mContext;
+        this.mData=mData;
+    }
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view;
+        LayoutInflater mInflater=LayoutInflater.from(mContext);
+        view= mInflater.inflate(R.layout.cardview_recipes,viewGroup,false);
+
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder myHolder, final int i) {
+
+         myHolder.recipeTitle.setText(mData.get(i).getRecipeName());
+         myHolder.img_recipe_thumbnail.setImageResource(mData.get(i).getThumbnail());
+         myHolder.cardView.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View v) {
+                 Intent intent=new Intent(mContext,RecipeActivity.class);
+                 intent.putExtra("RecipeName",mData.get(i).getRecipeName());
+                 intent.putExtra("RecipeIngredients",mData.get(i).getRecipeIngredients());
+                 intent.putExtra("RecipeMethodTitle",mData.get(i).getRecipeMethodTitle());
+                 intent.putExtra("Recipe",mData.get(i).getRecipe());
+                 intent.putExtra("Thumbnail",mData.get(i).getThumbnail());
+
+                 mContext.startActivity(intent);
+             }
+         });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+           TextView recipeTitle;
+           CardView cardView;
+           ImageView img_recipe_thumbnail;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            recipeTitle=(TextView)itemView.findViewById(R.id.recipe_text);
+            img_recipe_thumbnail=(ImageView)itemView.findViewById(R.id.recipe_img_id);
+            cardView=(CardView)itemView.findViewById(R.id.cardview_id);
+        }
+    }
+}
